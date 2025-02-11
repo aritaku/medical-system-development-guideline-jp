cf2c3400ffc170a8848e69179ae866d0f73503e7
aritaku <aritaku19@gmail.com>
2025-02-11 23:18:32 +0900

    readme 復活

--- hp/app/pages/[slug]/page.tsx
+++ hp/app/pages/[slug]/page.tsx
@@ -4,7 +4,6 @@ import matter from "gray-matter"
 import { remark } from "remark"
 import html from "remark-html"
 import Link from "next/link"
-import '../../globals.css'
 
 export async function generateStaticParams() {
   const files = fs.readdirSync(path.join(process.cwd(), "app/pages"))
@@ -15,11 +14,10 @@ export async function generateStaticParams() {
     }))
 }
 
-export default async function UpdatePage({ params }: { params: { slug: string } }) {
-  const { slug } = params
+export default async function Page({ params }: { params: { slug: string } }) {
+  const slug = await Promise.resolve(params.slug)
   const filePath = path.join(process.cwd(), "app/pages", `${slug}.md`)
 
-  // ファイルが存在しない場合のエラー処理
   if (!fs.existsSync(filePath)) {
     return (
       <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center p-4 sm:p-6 md:p-8">
@@ -28,7 +26,7 @@ export default async function UpdatePage({ params }: { params: { slug: string } 
             &larr; ホームに戻る
           </Link>
           <h1 className="text-3xl font-bold mb-4">ページが見つかりません</h1>
-          <p>申し訳ありませんが、指定された更新情報は存在しません。</p>
+          <p>申し訳ありませんが、指定されたページは存在しません。</p>
         </main>
       </div>
     )
