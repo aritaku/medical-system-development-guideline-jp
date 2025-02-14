95cd8fba8c94b9ab9a901e0afe9e812fe485657c
aritaku-da <TakArimur@digital.go.jp>
2025-02-14 09:35:05 +0900

    コンテンツ追加

--- hp/app/page.tsx
+++ hp/app/page.tsx
@@ -32,12 +32,28 @@ export default function Home() {
         <section className="mb-8">
           <h2 className="text-xl sm:text-2xl font-semibold mb-4">概要・目的</h2>
           <ul className="list-disc list-inside space-y-2 text-sm sm:text-base">
-            <li>クラウドを前提とした医療情報システムの開発ガイドラインを整備</li>
-            <li>医療機関におけるシステム運用負担の削減</li>
-            <li>サイバーセキュリティ対策の向上</li>
-            <li>医療情報のシームレスな活用</li>
-            <li>医療情報を活用した臨床試験・治験環境の活性化</li>
-            <li>日本の医療システム産業をオールジャパンで活性化</li>
+            <li>
+                <h3>目的</h3>
+                <p>本開発ガイドラインは、クラウドを前提とした医療情報システムの開発指針を提供します。</p>
+            </li>
+            <li>
+                <h3>期待される効果</h3>
+                <p>本ガイドラインに準拠した開発により、以下のメリットを社会全体で享受できます：</p>
+                <ul>
+                    <li>医療機関におけるシステム運用負担の削減</li>
+                    <li>サイバーセキュリティ対策の向上</li>
+                    <li>医療情報のシームレスな活用</li>
+                    <li>医療情報を活用した臨床試験・治験環境の活性化</li>
+                </ul>
+            </li>
+            <li>
+                <h3>ビジョン</h3>
+                <p>オープンソースとして管理・運用され、以下を目指します：</p>
+                <ul>
+                    <li>日本国内の医療情報システム開発企業の開発力向上</li>
+                    <li>医療システム産業のオールジャパンでの活性化</li>
+                </ul>
+            </li>
           </ul>
         </section>
 
@@ -46,6 +62,59 @@ export default function Home() {
           <p className="text-sm sm:text-base">
             診療所・病院及び、薬局や歯科などの医療機関において利用される全ての医療情報システム
           </p>
+        </section>
+
+        <section className="mb-8">
+          <h2 className="text-xl sm:text-2xl font-semibold mb-4">対象読者</h2>
+          <p className="text-sm sm:text-base">
+          既存の医療情報システムの開発企業及び、これから新規参入を検討しているベンチャー企業
+          </p>
+        </section>
+
+        <section className="mb-8">
+          <h2 className="text-xl sm:text-2xl font-semibold mb-4">対応関連法規</h2>
+          <p className="text-sm sm:text-base">
+          本ガイドラインに沿った開発を行うことで、以下の関連法きに対応できることを目指します。
+          </p>
+          <ul className="list-disc list-inside space-y-2 text-sm sm:text-base">
+            <li>個人情報保護法への対応</li>
+            <li>3省2ガイドラインへの対応</li>
+            <li>ISO/IEC 27001(ISMS)への対応</li>
+            <li>セキュリティ・バイ・デザインガイドラインへの対応</li>
+          </ul>
+        </section>
+
+        <section className="mb-8">
+          <h2 className="text-xl sm:text-2xl font-semibold mb-4">対象クラウドインフラ等(IaaS, PaaS)</h2>
+          <p className="text-sm sm:text-base">
+            以下のクラウドインフラを対象とします。
+          </p>
+          <ul className="list-disc list-inside space-y-2 text-sm sm:text-base">
+            <li>AWS</li>
+            <li>GCP</li>
+            <li>Azure</li>
+            <li>Cloudflare</li>
+            <li>Vercel</li>
+            <li>Supabase</li>
+          </ul>
+        </section>
+
+        <section className="mb-8">
+          <h2 className="text-xl sm:text-2xl font-semibold mb-4">対象言語及びフレームワーク</h2>
+          <p className="text-sm sm:text-base">
+            以下の言語及びフレームワークを対象とします。
+          </p>
+          <ul className="list-disc list-inside space-y-2 text-sm sm:text-base">
+            <li>フロントエンド</li>
+              <li>React.js</li>
+              <li>Next.js</li>
+            <li>バックエンド</li>
+              <li>TypeScript(Node.js)</li>
+              <li>Go</li>
+              <li>Rust</li>
+              <li>Ruby on Rails</li>
+              <li>PHP / Laravel</li>
+          </ul>
         </section>
 
         <section className="mb-8">
